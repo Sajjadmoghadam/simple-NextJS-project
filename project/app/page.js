1173dcdd84089e83c4d16dda84cd75f08cde97d7
Sajjadmoghadam <sajjad.moghadam1379@gmail.com>
2024-01-01 21:23:09 +0330

    create main home banner

--- project/app/page.js
+++ project/app/page.js
@@ -1,7 +1,16 @@
 import React from 'react'
-
+import banner from '@/public/assets/banner.jpg'
+import style from'./page.module.css'
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+    <div  >
+      <img className={style.banner} src={banner.src}/>
+
+
+    </div>
+    
+    
+    </>
   )
 }
